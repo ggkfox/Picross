e8324d5c6559b547928620f146e09380dd8abcd9
bduinkerk <43049255+bduinkerk@users.noreply.github.com>
2018-12-01 23:59:59 -0800

    op form

--- php/register.php
+++ php/register.php
@@ -3,13 +3,12 @@
 if (isset($_POST['register-submit'])) {
     include_once("connection.php");
 
-    if(isset($_POST["fname"]) && isset($_POST["lname"]) && isset($_POST["gender"]) && isset($_POST["username"]) && isset($_POST["avatar"]) && isset($_POST["password-verify"]) && isset($_POST["password"]) && isset($_POST["location"])) {
+    if(isset($_POST["fname"]) && isset($_POST["lname"]) && isset($_POST["gender"]) && isset($_POST["username"]) && isset($_POST["password-verify"]) && isset($_POST["password"]) && isset($_POST["location"])) {
 
         $errorMsg = "";
         $fname = $_POST["fname"];
         $lname = $_POST["lname"];
         $username = $_POST["username"];
-        $avatar = $_POST["avatar"];
         $gender = $_POST["gender"];
         $location = $_POST["location"];
 
@@ -20,20 +19,20 @@ if (isset($_POST['register-submit'])) {
             $errorMsg = $errorMsg . "Password doesnt match ";
         }
 
-        if(preg_match('/^[a-zA-z]*$/', $fname)) {
+        if(!preg_match('/^[a-zA-z]*$/', $fname)) {
             $errorMsg = $errorMsg . " Invalid First Name ";
         }
-        if(preg_match('/^[a-zA-z]*$/', $lname)) {
+        if(!preg_match('/^[a-zA-z]*$/', $lname)) {
             $errorMsg = $errorMsg . " Invalid Last Name ";
         }
-        if(preg_match('/^[a-zA-z]*$/', $location)) {
+        if(!preg_match('/^[a-zA-z]*$/', $location)) {
             $errorMsg = $errorMsg . " Invalid City ";
         }
 
         if(preg_match("/^[a-zA-z0-9]*$/", $username)) {
             $sql = "SELECT * FROM users WHERE username =?;";
             $stmt = mysqli_stmt_init($conn);
-            if (!mysqli_stmt_prepare($stmt, $sql)) {
+            if (mysqli_stmt_prepare($stmt, $sql)) {
                 $errorMsg = $errorMsg . " SQL error ";
             }
             else {
@@ -57,22 +56,59 @@ if (isset($_POST['register-submit'])) {
                 $errorMsg = $errorMsg . " SQL error ";
             }
             else {
-                $hashpass = password_hash($password, PASSWORD_DEFAULT);
-                mysqli_stmt_bind_param($stmt, "sssssss", $username, $fname, $lname, $hashpass, $gender, $location, $avatar);
-                mysqli_stmt_execute($stmt);
-                header("Location: ../title.html?signup=success");
+                $target_dir = "../uploads/";
+                $target_file = $target_dir . basename($_FILES["avatar"]["name"]);
+
+                $uploadOk = 1;
+
+                $imageFileType = pathinfo($target_file,PATHINFO_EXTENSION);
+                $check = getimagesize($_FILES["avatar"]["tmp_name"]);
+                if($check !== false) {
+                    $uploadOk = 1;
+                } else {
+                    $uploadOk = 0;
+                    $errorMsg = $errorMsg . " File not valid ";
+                    header("Location: ../title.html?error=".$errorMsg);
+                }
+                if (file_exists($target_file)) {
+                    $uploadOk = 0;
+                    $errorMsg = $errorMsg . " File not valid ";
+                    header("Location: ../title.html?error=".$errorMsg);
+                }   
+                if ($_FILES["avatar"]["size"] > 1000000) {
+                    $uploadOk = 0;
+                    $errorMsg = $errorMsg . " File not valid ";
+                    header("Location: ../title.html?error=".$errorMsg);
+                }
+                if($imageFileType != "jpg" && $imageFileType != "png") {
+                    $uploadOk = 0;
+                    $errorMsg = $errorMsg . " File not valid ";
+                    header("Location: ../title.html?error=".$errorMsg);
+                }
+                if ($uploadOk == 0) {
+                    $errorMsg = $errorMsg . " File not valid ";
+                    header("Location: ../title.html?error=".$errorMsg);
+                } 
+                else {
+                    if (move_uploaded_file($_FILES["avatar"]["tmp_name"], $target_file)) {
+                        $hashpass = password_hash($password, PASSWORD_DEFAULT);
+                        $avatar = $_FILES["avatar"]["name"];
+                        mysqli_stmt_bind_param($stmt, "sssssss", $username, $fname, $lname, $hashpass, $gender, $location, $avatar);
+                        mysqli_stmt_execute($stmt);
+        
+                        header("Location: ../title.html?signup=success");
+                    } else {
+                        $errorMsg = $errorMsg . " File didnt upload ";
+                        header("Location: ../title.html?error=".$errorMsg);
+                    }
+                }
             }
         }
         else {
             header("Location: ../title.html?error=".$errorMsg);
         }
      }
-
-    mysqli_stmt_close($stmt);
-    mysqli_close($conn);
 }
 else {
     header("Location: ../title.html");
 }
-
-//echo $fname . " " . $lname . " " . $username . " " . $avatar . " " . $gender . " " . $password;
