fe3a2e4be77a74382d5d6f4a5ba3b8626e195a7b
ggkfox <holland_74@hotmail.com>
2019-03-15 15:18:00 -0700

    final

--- index.php
+++ index.php
@@ -1,10 +1,10 @@
 <?php
 session_start();
 
-if(!isset($_SESSION['username']) || empty($_SESSION['username'])){
-	header("Location: ../title.php");
-  exit();
-}
+//if(!isset($_SESSION['username']) || empty($_SESSION['username'])){
+//	header("Location: ../title.php");
+//  exit();
+//}
 ?>
 
 <!DOCTYPE html>
